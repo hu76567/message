0fc1f2e1e1c58808b3b99d8fcc8a8d2d72b7e2ad
iq7ii <385897888@qq.com>
2020-02-15 22:48:09 +0800

    post接口测试

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 // 引入模块
 const express = require('express')
+const multer = require('multer')
 // 创建实例
 const app = express();
+// 托管静态资源
+app.use(express.static('user'))
+// 文件要存入的目录
+const upload = multer({ dest: 'avatars/' })
 
-app.use(express.static('begin'))
+//接口实现注册功能,用户信息放入database和头像保存在avatars文件夹
+// avatar文件键名
+app.post("/user_add", upload.single("avatar"), function (req, res) {
+    console.log(req.file)
+    console.log(req.body)
+    res.send("测试成功")
+})
 
 app.listen(8084, function () {
     console.log('服务已启动...')
