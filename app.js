d21a5da1176d76d0ee1e80edd9cafe28c3e38b8d
iq7ii <385897888@qq.com>
2020-02-18 22:14:17 +0800

    对json改对sql交互

--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(session(conf));
 // 引入封装好的模块
 const user = require('./untils/user')
 
+// 引入自定义模块用来操作mysql
+const dosql = require("./untils/dosql")
+
 // 托管静态资源
 app.use(express.static('user'))
 // 引用body-parser
@@ -30,52 +33,154 @@ const upload = multer({ dest: 'avatars/' })
 
 //接口实现注册功能,用户信息放入database和头像保存在avatars文件夹
 // avatar文件键名
-app.post("/user_add", upload.single("avatar"), function (req, res) {
+// app.post("/user_add", upload.single("avatar"), function (req, res) {
+//     // console.log(req.file)
+//     // console.log(req.body)
+
+//     //    操作数据
+//     let { name, pwd } = req.body;
+//     let avatarUrl = req.file.path;
+//     user.add(name, pwd, avatarUrl);
+//     res.send({
+//         code: 200,
+//         msg: "用户注册成功"
+//     })
+// })
+
+// 与数据库交互   注册
+app.post("/user_add_sql", upload.single("avatar"), function (req, res) {
     // console.log(req.file)
     // console.log(req.body)
 
     //    操作数据
     let { name, pwd } = req.body;
     let avatarUrl = req.file.path;
-    user.add(name, pwd, avatarUrl);
-    res.send({
-        code: 200,
-        msg: "用户注册成功"
-    })
-})
-
-
-// 用户登录  键值对 name，pwd   返回 登陆成功
-app.post("/user_login", (req, res) => {
-    let { name, pwd } = req.body;
-    // console.log(name, pwd)
-    let users = user.get();
-
-    let curUser = users.find(function (item) {
-        // console.log(item)
-        if (item.name === name && item.pwd === pwd) {
-            return true
+    // user.add(name, pwd, avatarUrl);
+    let sqlStr = `insert into  users(name,pwd)  values("${name}","${pwd}")`
+    dosql.doSql(sqlStr, function (err, data) {
+        if (err) {
+            // console.log(err)
+            res.send({
+                code: 400,
+                msg: "服务器端出错",
+            })
+        } else {
+            if (data.affectedRows === 1) {
+                res.send({
+                    code: 200,
+                    msg: "用户注册成功"
+                })
+            } else {
+                res.send({
+                    code: 200,
+                    msg: "用户注册失败"
+                })
+            }
         }
     })
-    console.log(curUser)
-    if (curUser) {
-        // 通过session发放凭证
-        req.session.isLogin = true;
-        req.session.name = name;
 
-        // 随后会给浏览器设置cookie,
-        res.send({
-            code: 200,
-            msg: "登陆成功",
-            data: curUser,
-        })
-    } else {
-        res.send({
-            code: 400,
-            msg: "用户名或密码错误,请重新输入",
-        })
-    }
 })
+
+
+
+
+
+// 对json文件操作
+// 用户登录  键值对 name，pwd   返回 登陆成功
+// app.post("/user_login", (req, res) => {
+//     let { name, pwd } = req.body;
+//     // console.log(name, pwd)
+//     let users = user.get();
+
+//     let curUser = users.find(function (item) {
+//         // console.log(item)
+//         if (item.name === name && item.pwd === pwd) {
+//             return true
+//         }
+//     })
+//     console.log(curUser)
+//     if (curUser) {
+//         // 通过session发放凭证
+//         req.session.isLogin = true;
+//         req.session.name = name;
+
+//         // 随后会给浏览器设置cookie,
+//         res.send({
+//             code: 200,
+//             msg: "登陆成功",
+//             data: curUser,
+//         })
+//     } else {
+//         res.send({
+//             code: 400,
+//             msg: "用户名或密码错误,请重新输入",
+//         })
+//     }
+// })
+
+// 操作sql  登陆
+
+app.post("/user_login_sql", (req, res) => {
+    // 1.解构赋值
+    let { name, pwd } = req.body;
+    // 2.根据name，pwd去数据库中搜索
+    // 找到登陆成功，找不到登陆失败
+    let sqlStr = ` select  name,pwd  from  users  where  name = "${name}" and pwd="${pwd}"`
+    dosql.doSql(sqlStr, function (err, data) {
+        if (err) {
+            console.log(err)
+            res.send({
+                code: 400,
+                msg: "服务器端出错",
+            })
+
+        } else {
+            console.log(data)
+            if (data.length > 0) {
+                // 通过session发放凭证
+                req.session.isLogin = true;
+                req.session.name = name;
+
+                // 随后会给浏览器设置cookie,
+                res.send({
+                    code: 200,
+                    msg: "登陆成功",
+                })
+            } else {
+                res.send({
+                    code: 400,
+                    msg: "用户名或密码错误,请重新输入",
+                })
+            }
+        }
+    })
+
+
+
+
+    // console.log(curUser)
+    // 返回值
+    // if (curUser) {
+    //     // 通过session发放凭证
+    //     req.session.isLogin = true;
+    //     req.session.name = name;
+
+    //     // 随后会给浏览器设置cookie,
+    //     res.send({
+    //         code: 200,
+    //         msg: "登陆成功",
+    //         data: curUser,
+    //     })
+    // } else {
+    //     res.send({
+    //         code: 400,
+    //         msg: "用户名或密码错误,请重新输入",
+    //     })
+    // }
+})
+
+
+
 
 // 获取用户信息,验证session
 app.get('/get_user', (req, res) => {
@@ -96,6 +201,8 @@ app.get('/get_user', (req, res) => {
     }
 })
 
+
+
 app.get('/quit', (req, res) => {
     req.session.destroy();
     res.send({
@@ -103,6 +210,9 @@ app.get('/quit', (req, res) => {
         msg: "quit"
     })
 })
+
+
+
 app.listen(8084, function () {
     console.log('服务器已启动...')
 })
